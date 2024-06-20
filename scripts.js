19241ef15b9c2a6bc08dce00b633b9bfacd01693
pvy-it-tdh <phucvy107@gmail.com>
2024-06-20 16:32:17 +0700

    update

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("playButton").addEventListener("click", function () {
+  var song = document.getElementById("birthdaySong");
+  song.play();
+  alert("Happy Birthday!");
+});
